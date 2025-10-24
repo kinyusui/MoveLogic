d417077ec06f2dda5d16a61d1751cec56721f701
kinyusui <kaixngchen@gmail.com>
2025-10-23 22:05:37 -0400

    Simplify ImportPath down to a simple dict.

--- src/Jscodeshift/UpdateImports/ImportPath.ts
+++ src/Jscodeshift/UpdateImports/ImportPath.ts
@@ -1,35 +1,12 @@
 import { ASTPath, CallExpression, ImportDeclaration } from "jscodeshift";
-import jscodeshift from "jscodeshift/src/core.js";
 
-export type EsmImportPath = ASTPath<ImportDeclaration>;
-export type CjsImportPath = ASTPath<CallExpression>;
-export type ImportPathG = EsmImportPath | CjsImportPath;
+type EsmImportPath = ASTPath<ImportDeclaration>;
+type CjsImportPath = ASTPath<CallExpression>;
+type ImportPathG = EsmImportPath | CjsImportPath;
 
-export interface ImportPath {
-  get path(): string;
-  set path(newPath: string);
-}
-
-class Esm implements ImportPath {
-  constructor(public props: { pathDict: EsmImportPath }) {}
-  get path() {
-    return this.props.pathDict.node.source.value as string;
-  }
-  set path(newPath: string) {
-    this.props.pathDict.node.source.value = newPath;
-  }
-}
-
-class Cjs implements ImportPath {
-  constructor(public props: { pathDict: CjsImportPath }) {}
-  get path() {
-    const importPath = this.props.pathDict.node.arguments[0] as any;
-    return (importPath?.value ?? "") as string;
-  }
-  set path(newPath: string) {
-    this.props.pathDict.node.arguments[0] = jscodeshift.stringLiteral(newPath);
-  }
-}
+export type ImportPath = {
+  path: string;
+};
 
 const isEsmImportPaths = (importPath: ImportPathG): importPath is EsmImportPath => {
   return importPath.node.type === "ImportDeclaration";
@@ -37,9 +14,10 @@ const isEsmImportPaths = (importPath: ImportPathG): importPath is EsmImportPath 
 
 export const configImportPath = ({ pathDict }: { pathDict: ImportPathG }) => {
   if (isEsmImportPaths(pathDict)) {
-    return new Esm({ pathDict: pathDict });
+    return { path: pathDict.node.source.value as string };
   } else {
-    return new Cjs({ pathDict: pathDict });
+    const node = pathDict.node.arguments[0] as any;
+    return { path: node.value as string };
   }
 };
 
